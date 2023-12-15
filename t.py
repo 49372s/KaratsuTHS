d600d2a33b3367e94522c324056364461e89645a
小鳥遊 <96430996+49372s@users.noreply.github.com>
2023-12-15 12:58:55 +0900

    Update t.py

--- t.py
+++ t.py
@@ -1,21 +1,25 @@
 #Import gpio library
 from gpiozero import Button
 from gpiozero import LED
-#sleep
+#apiリクエスト用モジュール
+import requests
+# sleep用モジュール
 import time
 
-#define variables
-button = Button(13)
-led = LED(19)
-on = False
+# 変数を宣言する
+button = Button(13)   #gpio 13を使用
+led = LED(19)         #gpio 19を使用
 
 #Mainroutine
 while True:
-  if button.is_pressed:
-    if on==False:
-      led.on()
-      on = True
-    else:
+  if ( loop == 10 ): #loop変数が10、つまり1秒経過したら状態をサーバーから取得する。本来はAsyncを用いて非同期処理をするべき
+    res = requests.get("https://kths.tkngh.jp/api/get/")
+    output = json.loads(res.text) #debug
+    if ( output['data'] == "no" ): # もし、状態がＯＦＦの場合、LEDを消灯して火災発生前の状態に戻す
       led.off()
-      on = False
-  time.sleep(1)
+    loop = 0 #loopは毎0にして、秒計測をする
+  if button.is_pressed: #ボタンが押されたら、LEDを点灯して、制御盤にリクエストを送信する
+    led.on()
+    requests.get("https://kths.tkngh.jp/api/control/?token=xxxxxxxx&mode=link")
+  time.sleep(.1)
+  loop = loop + 1
